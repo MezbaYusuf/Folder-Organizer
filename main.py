5b4254ac3c917387e1b4fc1ee2b46ea300fa871d
mezba <tyeaqub@gmail.com>
2026-08-16 18:11:38 +0600

    add extensions for all file types

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 import os 
 
+#image extensions
+image_extensions = (
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".webp",
+    ".tiff",
+    ".tif",
+    ".svg",
+    ".ico",
+    ".heic",
+    ".heif",
+    ".avif"
+)
+#music extension : 
+music_extensions = (
+    ".mp3",
+    ".wav",
+    ".flac",
+    ".aac",
+    ".ogg",
+    ".m4a",
+    ".wma",
+    ".opus",
+    ".aiff",
+    ".alac"
+)
+#document extensions : 
+document_extensions = (
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".txt",
+    ".rtf",
+    ".odt",
+    ".xls",
+    ".xlsx",
+    ".csv",
+    ".ppt",
+    ".pptx",
+    ".odp",
+    ".ods"
+)
 #listing the files
 files = os.listdir()
-print(files)
+print(type(files))
 
 #creating the 3 folders
 os.makedirs("Images", exist_ok=True)
 
 #checking the files last name : 
+
+for i in files : 
+    if i.endswith('.jpg') :
+        # print("Yes there is a file that ends with py")
+         
